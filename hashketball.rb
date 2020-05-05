829755b72621bcb777772f9654df6440ad141d41
Amer Fahmy <afahmy519@gmail.com>
2020-05-05 20:42:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,13 @@ def game_hash
     home: {
       team_name: ""
       colors: []
-      players: [{player_name: "", }]
+      players: [
+        {
+          player_name: "", 
+          
+          
+        }
+        ]
     }
     away: {}
     
